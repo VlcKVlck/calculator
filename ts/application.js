a6c6d0c61d2ff8dbd97aecf253185e2aa9eb5b40
V I <vi285@nyu.edu>
2022-11-22 16:46:08 +0200

    with sci buttons

--- ts/application.js
+++ ts/application.js
@@ -12,20 +12,11 @@ function loadApp() {
         document.getElementById("pagebody").className = theme;
         document.getElementById("title").style.fontFamily = font;
     }
+    scientific = false;
 }
 function settingsFunc() {
     alert("Developed by: Vicky \nVersion: 0.0 \nDescription: This is a simple calculator");
 }
-// //change themes
-// function changeTheme(){
-//     const a = document.getElementById("pagebody");
-//     if (a.className==='dark'){
-//         a.className='bright';
-//         }
-//     else  {
-//         a.className='dark';
-//         }
-// }
 //hide/show history
 function hideHistory() {
     var elem = document.getElementById("history");
@@ -61,27 +52,12 @@ function sciMode() {
         clearRes();
     }
     else {
-        elem.style.visibility = 'visible';
+        elem.style.visibility = 'hidden';
         btn.style.border = "inherit";
         scientific = false;
         clearRes();
     }
 }
-// function siteConfig (){
-//     let siteBG = document.getElementById("site").style.backgroundColor;
-//     if (color =='red'){
-//         document.getElementById("site").style.backgroundColor ="red";
-//
-//     }
-//
-// }
 document.getElementById('lightbulb').addEventListener("click", lightScreen);
 document.getElementById('sciMode').addEventListener("click", sciMode);
 document.addEventListener('DOMContentLoaded', loadApp);
-// document.querySelector('submit').addEventListener(onsubmit());
-// const bgColors = document.querySelectorAll('option');
-// bgColors.forEach(function(bgColor){
-//     bgColor.addEventListener('select', () => changeColor(bgColor.getAttribute('value')
-// })
-// const a = document.querySelector("#backgroundcolors").getAttribute('value');
-// console.log (a)
